3b5215d392ed957181af90fff3cdafff54641b6a
manthankanjiya <manthankanjiya24@gmail.com>
2025-07-04 10:13:18 +0530

    commit

---
+++ Extra Practice/comF.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class comF 
+{
+public static void main(String[] args) 
+{
+// A = wl (area of rectangle)
+	
+	float w=15.6f, l=18.3f,ans;
+	
+	ans = w*l;
+	
+	System.out.println("Answer = "+ans);
+	
+}
+}
---
+++ Extra Practice/come.java
@@ -0,0 +1,16 @@
+package Practice;
+
+public class come 
+{
+public static void main(String[] args) 
+{
+//A=a*a (area of square)
+	
+	int a=20,ans;
+	
+	ans = a*a;
+	
+	System.out.println("A = "+ans);
+
+}
+}
